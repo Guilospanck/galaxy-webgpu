1ffca3c6fb66edb3f7895a03fbd1c00f3bf3240e
Guilherme Pereira <guilospanck@protonmail.com>
2025-01-11 16:31:24 +0000

    Add forked stats.js

--- src/planet.ts
+++ src/planet.ts
@@ -18,6 +18,7 @@ import planetWGSL from "./shaders/planet.wgsl?raw";
 import Stats from "stats.js";
 
 const stats = new Stats();
+
 stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom);
 
